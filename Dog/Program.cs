47709914a3947c36fae247a58941b99daaf8affb
hrustalevanv <hrustalevanv2021@gmail.com>
2021-11-05 18:37:50 +0300

    Add task dog

---
+++ Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿ouble OneFriends = 1;
+double SecondFriends = 2;
+double DogSpeed = 5;
+double distance = 10000;
+double distance0 = 5;
+int friend = 2;
+int count = 0;
+double time = 0;
+
+while(distance0 < distance)
+{
+    if (friend == 2)
+    {
+        time = distance/(OneFriends + DogSpeed);
+        friend = 1;
+    }
+else
+    {
+        time = distance/(SecondFriends + DogSpeed);
+        friend = 2;
+    }
+    distance = distance - (OneFriends + SecondFriends) * time;
+    Console.WriteLine($"Расстояние {count ++} раз");
+    Console.WriteLine($"дистанция = {distance:f2}");
+    count ++;
+}
